7bbe54e9ef255766314f57fdaa270ea0337a8f2d
ano333333 <ano333333@github.gmail.com>
2024-05-29 04:01:37 +0900

    2-3

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,48 @@
-use plotters::prelude::*;
-use std::error::Error;
+use ndarray::prelude::*;
+fn separator() -> String {
+    return (0..20)
+        .map(|_| String::from("-"))
+        .collect::<Vec<String>>()
+        .concat();
+}
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let root = BitMapBackend::new("images/0.png", (640, 480)).into_drawing_area();
-    root.fill(&WHITE)?;
-    let x_min = 0f32;
-    let x_max = 6.0f32;
-    let y_min = -1.0f32;
-    let y_max = 1.0f32;
-    let x_iter = (0..=1200).map(|x| x as f32 / 200.0);
-    let mut chart = ChartBuilder::on(&root)
-        .caption("y=sin(x)&cos(x)", ("Arial", 50).into_font())
-        .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(x_min..x_max, y_min..y_max)?;
-    chart.configure_mesh().draw()?;
-    let sin_iter = x_iter.clone().map(|x| x.sin());
-    let x_sin_iter = x_iter.zip(sin_iter);
-    chart
-        .draw_series(LineSeries::new(x_sin_iter, &RED))?
-        .label("y = sin(x)")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-    let x_iter = (0..=1200).map(|x| x as f32 / 200.0);
-    let cos_iter = x_iter.clone().map(|x| x.cos());
-    let x_cos_iter = x_iter.zip(cos_iter);
-    chart
-        .draw_series(LineSeries::new(x_cos_iter, &BLUE))?
-        .label("y = cos(x)")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
-    chart
-        .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
-        .draw()?;
-    root.present()?;
+fn and(x1: f64, x2: f64) -> i32 {
+    let x = array![x1, x2];
+    let w = array![0.5, 0.5];
+    let b = -0.7;
+    let y = (x * w).sum() + b;
+    return if y <= 0.0 { 0 } else { 1 };
+}
 
-    Ok(())
+fn nand(x1: f64, x2: f64) -> i32 {
+    let x = array![x1, x2];
+    let w = array![-0.5, -0.5];
+    let b = 0.7;
+    let y = (x * w).sum() + b;
+    return if y <= 0.0 { 0 } else { 1 };
+}
+
+fn or(x1: f64, x2: f64) -> i32 {
+    let x = array![x1, x2];
+    let w = array![0.5, 0.5];
+    let b = -0.2;
+    let y = (x * w).sum() + b;
+    return if y <= 0.0 { 0 } else { 1 };
+}
+
+fn main() {
+    println!("AND(0, 0) = {}", and(0.0, 0.0));
+    println!("AND(1, 0) = {}", and(1.0, 0.0));
+    println!("AND(0, 1) = {}", and(0.0, 1.0));
+    println!("AND(1, 1) = {}", and(1.0, 1.0));
+    println!("{}", separator());
+    println!("NAND(0, 0) = {}", nand(0.0, 0.0));
+    println!("NAND(1, 0) = {}", nand(1.0, 0.0));
+    println!("NAND(0, 1) = {}", nand(0.0, 1.0));
+    println!("NAND(1, 1) = {}", nand(1.0, 1.0));
+    println!("{}", separator());
+    println!("OR(0, 0) = {}", or(0.0, 0.0));
+    println!("OR(1, 0) = {}", or(1.0, 0.0));
+    println!("OR(0, 1) = {}", or(0.0, 1.0));
+    println!("OR(1, 1) = {}", or(1.0, 1.0));
 }
