b7a621f71f2c1ac34942375bc1b8f7ca132221bd
ano333333 <ano333333@github.gmail.com>
2024-05-30 05:16:06 +0900

    3-5

--- src/main.rs
+++ src/main.rs
@@ -21,23 +21,35 @@ fn identity_function<D: Dimension>(x: ArrayView<f64, D>) -> Array<f64, D> {
     x.to_owned()
 }
 
+fn softmax<D: Dimension>(x: ArrayView<f64, D>) -> Array<f64, D> {
+    let max = x.fold(0.0 / 0.0, |acc, &x| x.max(acc));
+    let c = x.mapv(|x| (x - max).exp()).sum();
+    x.mapv(|x| (x - max).exp() / c)
+}
+
 struct Network {
     w1: Array2<f64>,
     b1: Array1<f64>,
+    act1: fn(ArrayView2<f64>) -> Array2<f64>,
     w2: Array2<f64>,
     b2: Array1<f64>,
+    act2: fn(ArrayView2<f64>) -> Array2<f64>,
     w3: Array2<f64>,
     b3: Array1<f64>,
+    act3: fn(ArrayView2<f64>) -> Array2<f64>,
 }
 
 fn init_network() -> Network {
     Network {
         w1: array![[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]],
         b1: array![0.1, 0.2, 0.3],
+        act1: sigmoid,
         w2: array![[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]],
         b2: array![0.1, 0.2],
+        act2: sigmoid,
         w3: array![[0.1, 0.3], [0.2, 0.4]],
         b3: array![0.1, 0.2],
+        act3: identity_function,
     }
 }
 
@@ -52,16 +64,22 @@ fn init_network() -> Network {
 
 fn forward(network: &Network, x: ArrayView2<f64>) -> Array2<f64> {
     let a1 = x.dot(&network.w1) + &network.b1;
-    // let z1 = sigmoid(a1.view());
-    let z1 = relu(a1.view());
+    let z1 = (network.act1)(a1.view());
     let a2 = z1.dot(&network.w2) + &network.b2;
-    // let z2 = sigmoid(a2.view());
-    let z2 = relu(a2.view());
+    let z2 = (network.act2)(a2.view());
     let a3 = z2.dot(&network.w3) + &network.b3;
-    identity_function(a3.view())
+    (network.act3)(a3.view())
 }
 
 fn main() {
+    let a = array![0.3, 2.9, 4.0];
+    let y = softmax(a.view());
+    println!("y: {:?}", y);
+    println!("{}", separator());
+    let a = array![1010.0, 1000.0, 990.0];
+    let y = softmax(a.view());
+    println!("y: {:?}", y);
+    println!("{}", separator());
     let network = init_network();
     // let x = array![1.0, 0.5];
     // let y = forward(&network, x.view());
@@ -75,35 +93,4 @@ fn main() {
     let y = forward(&network, x.view());
     println!("y: {:?}", y);
     println!("{}", separator());
-
-    let root = BitMapBackend::new("images/forward.png", (640, 480)).into_drawing_area();
-    root.fill(&WHITE).unwrap();
-    let mut chart = ChartBuilder::on(&root)
-        .caption("Forward", ("sans-serif", 30).into_font())
-        .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(-1.0..1.0, -1.0..1.0)
-        .unwrap();
-    chart.configure_mesh().draw().unwrap();
-
-    // 直線x = -5,-4,...,4,5がforwardによりどのように移動するか?
-    for x_coord in -5..=5 {
-        // 行列xの各行は(x_coord,100.0)から(x_coord,-100.0)
-        let x_col1 = Array::range(-100.0, 100.0, 0.1);
-        let x_col0 = Array::from_elem(x_col1.len(), x_coord as f64);
-        let x = stack![Axis(1), x_col0, x_col1];
-        let y = forward(&network, x.view());
-        println!("x: {:?}", x);
-        println!("y: {:?}", y);
-        // y[0],y[1],...,y[-1]を描画
-        chart
-            .draw_series(LineSeries::new(
-                (0..y.shape()[0]).map(|i| (y[[i, 0]], y[[i, 1]])),
-                &BLACK,
-            ))
-            .unwrap();
-        println!("{}", separator());
-    }
-    root.present().unwrap();
 }
