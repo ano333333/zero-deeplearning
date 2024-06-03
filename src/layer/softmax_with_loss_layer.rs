2584e0150ccfee7998805ee0e2ba8255ec3a97a9
菊池雅英 <m20000406k@gmail.com>
2024-06-03 10:36:34 +0900

    5-7

--- src/layer/softmax_with_loss_layer.rs
+++ src/layer/softmax_with_loss_layer.rs
@@ -2,23 +2,23 @@ use crate::layer::layer::Layer;
 use crate::subfunction::{cross_entropy_error::cross_entropy_error, softmax_batch::softmax_batch};
 use ndarray::prelude::Array2;
 
-pub struct SoftmaxWithLossLayer<'a> {
+pub struct SoftmaxWithLossLayer {
     loss: f64,
     y: Array2<f64>,
-    t: &'a Array2<f64>,
+    t: Array2<f64>,
 }
 
-impl<'a> SoftmaxWithLossLayer<'a> {
-    pub fn new(t: &'a Array2<f64>) -> Self {
+impl SoftmaxWithLossLayer {
+    pub fn new(t: &Array2<f64>) -> Self {
         SoftmaxWithLossLayer {
             loss: 0.0,
             y: Array2::zeros((0, 0)),
-            t,
+            t: t.clone(),
         }
     }
 }
 
-impl<'a> Layer<Array2<f64>, f64> for SoftmaxWithLossLayer<'a> {
+impl Layer<Array2<f64>, f64> for SoftmaxWithLossLayer {
     fn forward(&mut self, y: &Array2<f64>) -> f64 {
         self.y = softmax_batch(y.view());
         self.loss = cross_entropy_error(self.y.view(), self.t.view());
@@ -26,6 +26,6 @@ impl<'a> Layer<Array2<f64>, f64> for SoftmaxWithLossLayer<'a> {
     }
     fn backward(&mut self, _: &f64) -> Array2<f64> {
         let batch_size = self.t.shape()[0] as f64;
-        (self.y - self.t) / batch_size
+        (self.y.clone() - self.t.clone()) / batch_size
     }
 }
