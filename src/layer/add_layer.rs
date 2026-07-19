7b88d868cb105d2bc36138b1e16a9cf966c4e4fc
ano3 <ano333333github@gmail.com>
2026-07-20 04:30:36 +0900

    test: AddLayer/MulLayerのテスト

--- src/layer/add_layer.rs
+++ src/layer/add_layer.rs
@@ -21,3 +21,34 @@ impl<Dim: Dimension> Layer<(Array<f64, Dim>, Array<f64, Dim>), Array<f64, Dim>> 
         (dout.clone(), dout.clone())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ndarray::array;
+
+    #[test]
+    fn forward_adds_fixed_three_by_three_matrices_elementwise() {
+        let mut layer = AddLayer::new();
+        let x = array![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]];
+        let y = array![[9.0, 8.0, 7.0], [6.0, 5.0, 4.0], [3.0, 2.0, 1.0]];
+
+        let out = layer.forward(&(x, y));
+
+        assert_eq!(
+            out,
+            array![[10.0, 10.0, 10.0], [10.0, 10.0, 10.0], [10.0, 10.0, 10.0]]
+        );
+    }
+
+    #[test]
+    fn backward_returns_upstream_gradient_for_both_inputs() {
+        let mut layer = AddLayer::new();
+        let dout = array![[1.0, 0.5, 2.0], [1.5, 1.0, 0.25], [0.0, 3.0, 4.0]];
+
+        let (dx, dy) = layer.backward(&dout);
+
+        assert_eq!(dx, dout);
+        assert_eq!(dy, dout);
+    }
+}
--- src/layer/mul_layer.rs
+++ src/layer/mul_layer.rs
@@ -25,3 +25,43 @@ impl<Dim: Dimension> Layer<(Array<f64, Dim>, Array<f64, Dim>), Array<f64, Dim>> 
         (dout * &self.y, dout * &self.x)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ndarray::array;
+
+    #[test]
+    fn forward_multiplies_fixed_three_by_three_matrices_elementwise() {
+        let mut layer = MulLayer::new();
+        let x = array![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]];
+        let y = array![[9.0, 8.0, 7.0], [6.0, 5.0, 4.0], [3.0, 2.0, 1.0]];
+
+        let out = layer.forward(&(x, y));
+
+        assert_eq!(
+            out,
+            array![[9.0, 16.0, 21.0], [24.0, 25.0, 24.0], [21.0, 16.0, 9.0]]
+        );
+    }
+
+    #[test]
+    fn backward_uses_saved_three_by_three_inputs_for_gradients() {
+        let mut layer = MulLayer::new();
+        let x = array![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]];
+        let y = array![[9.0, 8.0, 7.0], [6.0, 5.0, 4.0], [3.0, 2.0, 1.0]];
+        let dout = array![[1.0, 0.5, 2.0], [1.5, 1.0, 0.25], [0.0, 3.0, 4.0]];
+
+        layer.forward(&(x, y));
+        let (dx, dy) = layer.backward(&dout);
+
+        assert_eq!(
+            dx,
+            array![[9.0, 4.0, 14.0], [9.0, 5.0, 1.0], [0.0, 6.0, 4.0]]
+        );
+        assert_eq!(
+            dy,
+            array![[1.0, 1.0, 6.0], [6.0, 5.0, 1.5], [0.0, 24.0, 36.0]]
+        );
+    }
+}
