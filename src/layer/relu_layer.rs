95478d436d0dd7a8be71a3b7dcf3a62ec5425753
ano3 <ano333333github@gmail.com>
2026-07-24 01:43:21 +0900

    test: ReluLayerのテスト追加

--- src/layer/relu_layer.rs
+++ src/layer/relu_layer.rs
@@ -22,3 +22,64 @@ impl<Dim: Dimension> Layer<Array<f64, Dim>, Array<f64, Dim>> for ReluLayer<Dim> 
         dout * &self.mask
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::subfunction::relu::relu;
+    use ndarray::array;
+
+    const EPSILON: f64 = 1e-6;
+
+    #[test]
+    fn forward_keeps_positive_inputs() {
+        let mut layer = ReluLayer::new();
+        let x = array![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
+
+        let out = layer.forward(&x);
+
+        assert_eq!(out, x);
+    }
+
+    #[test]
+    fn forward_zeroes_negative_inputs() {
+        let mut layer = ReluLayer::new();
+        let x = array![-0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9, -1.0];
+
+        let out = layer.forward(&x);
+
+        assert_eq!(out, Array::zeros(x.raw_dim()));
+    }
+
+    #[test]
+    fn backward_matches_central_difference_for_positive_inputs() {
+        let mut layer = ReluLayer::new();
+        let x = array![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
+        let dout = Array::ones(x.raw_dim());
+        let numerical_gradient =
+            (relu((&x + EPSILON).view()) - relu((&x - EPSILON).view())) / (2.0 * EPSILON);
+
+        layer.forward(&x);
+        let backward_gradient = layer.backward(&dout);
+
+        for (actual, expected) in backward_gradient.iter().zip(numerical_gradient.iter()) {
+            assert!((actual - expected).abs() < EPSILON);
+        }
+    }
+
+    #[test]
+    fn backward_matches_central_difference_for_negative_inputs() {
+        let mut layer = ReluLayer::new();
+        let x = array![-0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9, -1.0];
+        let dout = Array::ones(x.raw_dim());
+        let numerical_gradient =
+            (relu((&x + EPSILON).view()) - relu((&x - EPSILON).view())) / (2.0 * EPSILON);
+
+        layer.forward(&x);
+        let backward_gradient = layer.backward(&dout);
+
+        for (actual, expected) in backward_gradient.iter().zip(numerical_gradient.iter()) {
+            assert!((actual - expected).abs() < EPSILON);
+        }
+    }
+}
