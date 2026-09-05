311b632336e329fcd7660e645bd693d505fb1fa1
ano3 <ano333333github@gmail.com>
2026-09-05 20:32:14 +0900

    test: AffineLayerのテスト

--- src/layer/affine_layer.rs
+++ src/layer/affine_layer.rs
@@ -35,3 +35,77 @@ impl<'a> Layer<Array2<f64>, Array2<f64>> for AffineLayer<'a> {
         dout.dot(&self.w.t())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ndarray::array;
+
+    const H: f64 = 1e-6;
+    const EPSILON: f64 = 1e-6;
+
+    fn assert_close(actual: f64, expected: f64) {
+        assert!(
+            (actual - expected).abs() < EPSILON,
+            "expected {expected}, got {actual}"
+        );
+    }
+
+    #[test]
+    fn forward_matches_direct_affine_calculation() {
+        let x = array![[0.7, -1.2, 2.3]];
+        let w = array![[-0.4], [1.5], [0.8]];
+        let b = array![-0.6];
+        let expected = x.dot(&w) + &b;
+        let mut layer = AffineLayer::new(&w, &b);
+
+        let actual = layer.forward(&x);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn backward_matches_central_difference_for_all_inputs_and_parameters() {
+        let x = array![[0.7, -1.2, 2.3]];
+        let w = array![[-0.4], [1.5], [0.8]];
+        let b = array![-0.6];
+        let dout = array![[1.0]];
+        let mut layer = AffineLayer::new(&w, &b);
+
+        layer.forward(&x);
+        let dx = layer.backward(&dout);
+
+        for (index, _) in x.indexed_iter() {
+            let mut x_plus = x.clone();
+            let mut x_minus = x.clone();
+            x_plus[index] += H;
+            x_minus[index] -= H;
+
+            let plus = AffineLayer::new(&w, &b).forward(&x_plus)[[0, 0]];
+            let minus = AffineLayer::new(&w, &b).forward(&x_minus)[[0, 0]];
+            assert_close(dx[index], (plus - minus) / (2.0 * H));
+        }
+
+        for (index, _) in w.indexed_iter() {
+            let mut w_plus = w.clone();
+            let mut w_minus = w.clone();
+            w_plus[index] += H;
+            w_minus[index] -= H;
+
+            let plus = AffineLayer::new(&w_plus, &b).forward(&x)[[0, 0]];
+            let minus = AffineLayer::new(&w_minus, &b).forward(&x)[[0, 0]];
+            assert_close(layer.dw[index], (plus - minus) / (2.0 * H));
+        }
+
+        for (index, _) in b.indexed_iter() {
+            let mut b_plus = b.clone();
+            let mut b_minus = b.clone();
+            b_plus[index] += H;
+            b_minus[index] -= H;
+
+            let plus = AffineLayer::new(&w, &b_plus).forward(&x)[[0, 0]];
+            let minus = AffineLayer::new(&w, &b_minus).forward(&x)[[0, 0]];
+            assert_close(layer.db[index], (plus - minus) / (2.0 * H));
+        }
+    }
+}
