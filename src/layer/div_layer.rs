296ebc88c03f04e9e04b3bdc485e62ac2cfd39e5
ano3 <ano333333github@gmail.com>
2026-09-08 01:48:33 +0900

    feat: DivLayerに実行自動的サイズチェック追加

--- src/layer/div_layer.rs
+++ src/layer/div_layer.rs
@@ -1,6 +1,8 @@
 use crate::layer::layer::Layer;
 use ndarray::{prelude::Array, Dimension};
 
+const MIN_ABSOLUTE_VALUE: f64 = 1e-12;
+
 pub struct DivLayer<Dim: Dimension> {
     x: Array<f64, Dim>,
 }
@@ -15,10 +17,19 @@ impl<Dim: Dimension> DivLayer<Dim> {
 
 impl<Dim: Dimension> Layer<Array<f64, Dim>, Array<f64, Dim>> for DivLayer<Dim> {
     fn forward(&mut self, x: &Array<f64, Dim>) -> Array<f64, Dim> {
+        assert!(
+            x.iter().all(|value| value.abs() >= MIN_ABSOLUTE_VALUE),
+            "all elements of x must have an absolute value of at least 1e-12"
+        );
         self.x = x.clone();
         1.0 / x
     }
     fn backward(&mut self, dout: &Array<f64, Dim>) -> Array<f64, Dim> {
+        assert_eq!(
+            dout.shape(),
+            self.x.shape(),
+            "dout and x must have the same shape"
+        );
         -dout / (&self.x * &self.x)
     }
 }
@@ -45,6 +56,25 @@ mod tests {
     }
 
     #[test]
+    #[should_panic(expected = "all elements of x must have an absolute value of at least 1e-12")]
+    fn forward_rejects_elements_smaller_than_minimum_absolute_value() {
+        let mut layer = DivLayer::new();
+        let x = array![[1.0, 0.5e-12], [-1.0, -2.0]];
+
+        layer.forward(&x);
+    }
+
+    #[test]
+    fn forward_accepts_elements_at_minimum_absolute_value() {
+        let mut layer = DivLayer::new();
+        let x = array![[1e-12, -1e-12]];
+
+        let actual = layer.forward(&x);
+
+        assert_eq!(actual, array![[1e12, -1e12]]);
+    }
+
+    #[test]
     fn backward_matches_central_difference() {
         let mut layer = DivLayer::new();
         let x = array![[0.5, -1.0, 2.0], [4.0, -5.0, 10.0]];
@@ -59,5 +89,16 @@ mod tests {
         for (actual, expected) in backward_gradient.iter().zip(numerical_gradient.iter()) {
             assert!((actual - expected).abs() < EPSILON);
         }
+    }
+
+    #[test]
+    #[should_panic(expected = "dout and x must have the same shape")]
+    fn backward_rejects_gradient_with_different_shape_from_input() {
+        let mut layer = DivLayer::new();
+        let x = array![[0.5, -1.0, 2.0], [4.0, -5.0, 10.0]];
+        let dout = array![[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]];
+        layer.forward(&x);
+
+        layer.backward(&dout);
     }
 }
